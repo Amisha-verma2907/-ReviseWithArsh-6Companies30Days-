4f73098d1470f55071a571019cdd6a16fa7c4489
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-01-05 15:34:30 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (98.34%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMax(vector<int>&dp,vector<int>& nums,int ind){
+        if(ind==0){
+            return nums[ind];
+        }
+        if(ind<0){
+            return 0;
+        }
+        if(dp[ind]!=-1)
+        {
+            return dp[ind];
+        }
+        
+        int pick = nums[ind]+findMax(dp,nums,ind-2);
+        int nonPick =  0 + findMax(dp,nums,ind-1);
+        
+        return dp[ind]=max(pick,nonPick);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        return findMax(dp,nums,n-1);
+    }
+};
