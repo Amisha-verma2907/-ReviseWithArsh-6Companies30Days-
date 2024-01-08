36d8cef91216cee5f2e9ba818e8e9c577e518d82
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-01-08 23:25:42 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (56.23%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int solve(int n,int k){
+    int solve(vector<int>&dp,int n,int k){
         if(n==1){
             return 0;
         }
-        return (solve(n-1,k)+k)%n;
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        return dp[n]=(solve(dp,n-1,k)+k)%n;
     }
     int findTheWinner(int n, int k) {
         //in modulus 0 based indexing is there therefore we need to add 1
-        int ans=solve(n,k)+1;
+        vector<int>dp(n+1,-1);
+        int ans=solve(dp,n,k)+1;
         return ans;
     }
 };
