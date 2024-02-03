9be26204a4005be80c67ef83120457884f1fff04
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-02-03 19:56:33 +0530

    Time: 40 ms (82.44%), Space: 27 MB (8.95%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-         map<string,int> arr;
-        if(s.size() < 10)
-            return {};
-        for(int i = 0; i < s.size()-9; ++i){
-            string str = s.substr(i,10);
-            arr[str]++;
-        }
+//          map<string,int> arr;
+//         if(s.size() < 10)
+//             return {};
+//         for(int i = 0; i < s.size()-9; ++i){
+//             string str = s.substr(i,10);
+//             arr[str]++;
+//         }
         
-        vector<string> ans;
-        for(auto x: arr){
-            if(x.second > 1)
-            ans.push_back(x.first);
-        }
+//         vector<string> ans;
+//         for(auto x: arr){
+//             if(x.second > 1)
+//             ans.push_back(x.first);
+//         }
         
-        return ans;
+//         return ans;
+    vector<string>v;
+        if(s.length()<=10)
+        {
+            return v;
+        }
+        unordered_map<string,int>mp;
+        for(int i=0;i<=s.length()-10;i++)
+        {
+            string h = s.substr(i,10);
+            mp[h]++;
+        }
+        for(auto x:mp)
+        {
+            if(x.second>1)
+            {
+                v.push_back(x.first);
+                
+            }
+        }
+        return v;
     }
 };
