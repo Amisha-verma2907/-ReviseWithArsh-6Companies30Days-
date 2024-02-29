deece60bd8ca0643f6a09caadf3f88d22715196b
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-02-29 23:44:54 +0530

    Time: 15 ms (18.41%), Space: 10.8 MB (66.67%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        string ans="";
+        
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+        }
+        priority_queue<pair<int,char>>pq;
+        for(auto i:mp)
+        {
+            pq.push({i.second,i.first});
+        }
+        while(!pq.empty())
+        {
+            int n=pq.top().first;
+            while(n--)
+            {
+                ans+=pq.top().second;
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
