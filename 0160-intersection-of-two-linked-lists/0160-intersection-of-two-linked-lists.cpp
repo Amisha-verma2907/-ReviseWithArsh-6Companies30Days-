658b01b090cdc5bfaad9cc7da1bcebdc70514d14
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-03-22 23:14:26 +0530

    Time: 37 ms (72.75%), Space: 17.1 MB (34.80%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,24 +8,35 @@
  */
 class Solution {
 public:
+    ListNode* collision(ListNode* node1,ListNode*node2,int diff){
+        while(diff--)
+        {
+            node2=node2->next;
+        }
+        while(node1!=node2){
+            node1=node1->next;
+            node2=node2->next;
+        }
+        return node1;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
         //using hashing in this time complexity and space complexity is more
-        unordered_map<ListNode*,int>mp;
-        ListNode * temp=headA;
-        while(temp!=NULL){
-            mp[temp]=1;
-            temp=temp->next;
-        }
-        temp=headB;
-        while(temp!=NULL){
-        if(mp.find(temp)!=mp.end()){
-            return temp;
-        }
-            temp=temp->next;
-            }
+//         unordered_map<ListNode*,int>mp;
+//         ListNode * temp=headA;
+//         while(temp!=NULL){
+//             mp[temp]=1;
+//             temp=temp->next;
+//         }
+//         temp=headB;
+//         while(temp!=NULL){
+//         if(mp.find(temp)!=mp.end()){
+//             return temp;
+//         }
+//             temp=temp->next;
+//             }
         
-        return NULL;
+//         return NULL;
      
         
         //using second approach sc:O(1) and TC:O(N1+2N2);
@@ -42,26 +53,16 @@ public:
             N2++;
         }
         
-        if(N2>N1){
-            temp2=headB;
-            int diff=N2-N1;
-            while(diff--){
-               temp2=temp2->next; 
-            }
-        }else{
-            temp1=headA;
-            int diff=N1-N2;
-            while(diff--){
-                temp1=temp1->next;
-            }
-        }
-        
+        if(N2>N1){           //O(N2-N1)
+           return collision(headA,headB,N2-N1);
             
-       while(temp1!=temp2){
-           temp1=temp1->next;
-           temp2=temp2->next;
-       }
-        return temp1;
+        }else{
+            return collision(headB,headA,N1-N2);
+            }
+        
+        return NULL;
+            
+       
         
     }
 };
