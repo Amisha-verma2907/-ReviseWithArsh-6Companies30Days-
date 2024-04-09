e56fb0245561153b389b6c32be8ddd70bbbce1f9
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-04-09 16:05:40 +0530

    Time: 39 ms (60.29%), Space: 17 MB (69.41%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,17 +8,17 @@
  */
 class Solution {
 public:
-    ListNode* collision(ListNode* node1,ListNode*node2,int diff){
-        while(diff--)
-        {
-            node2=node2->next;
-        }
-        while(node1!=node2){
-            node1=node1->next;
-            node2=node2->next;
-        }
-        return node1;
-    }
+    // ListNode* collision(ListNode* node1,ListNode*node2,int diff){
+    //     while(diff--)
+    //     {
+    //         node2=node2->next;
+    //     }
+    //     while(node1!=node2){
+    //         node1=node1->next;
+    //         node2=node2->next;
+    //     }
+    //     return node1;
+    // }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
         //using hashing in this time complexity and space complexity is more
@@ -40,29 +40,54 @@ public:
      
         
         //using second approach sc:O(1) and TC:O(N1+2N2);
-        ListNode * temp1=headA;
-        ListNode * temp2=headB;
-        int N1=0,N2=0;
-        while(temp1!=NULL){ //O(N1) N1=cnt1;
-           N1++;
+//         ListNode * temp1=headA;
+//         ListNode * temp2=headB;
+//         int N1=0,N2=0;
+//         while(temp1!=NULL){ //O(N1) N1=cnt1;
+//            N1++;
+//             temp1=temp1->next;
+//         }
+        
+//         while(temp2!=NULL){  //O(N2) N2=cnt2
+//             temp2=temp2->next;
+//             N2++;
+//         }
+        
+//         if(N2>N1){           //O(N2-N1)
+//            return collision(headA,headB,N2-N1);
+            
+//         }else{
+//             return collision(headB,headA,N1-N2);
+//             }
+        
+//         return NULL;
+        
+        
+        //using third appraoch
+        if(headA==NULL || headB==NULL){
+            return NULL;
+        }
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+        
+        while(temp1!=temp2){
             temp1=temp1->next;
-        }
-        
-        while(temp2!=NULL){  //O(N2) N2=cnt2
             temp2=temp2->next;
-            N2++;
-        }
-        
-        if(N2>N1){           //O(N2-N1)
-           return collision(headA,headB,N2-N1);
             
-        }else{
-            return collision(headB,headA,N1-N2);
+            if(temp1==temp2){
+                return temp1;
             }
-        
-        return NULL;
             
-       
+            if(temp1==NULL)
+            {
+                temp1=headB;
+            }
+            if(temp2==NULL){
+                temp2=headA;
+            }
+        }
+            
+       return temp1;
         
     }
 };
