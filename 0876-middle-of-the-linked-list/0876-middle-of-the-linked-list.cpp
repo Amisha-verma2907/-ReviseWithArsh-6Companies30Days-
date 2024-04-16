9b221ea0cba2bd66c071747d8496af484c0c7891
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-04-17 00:30:32 +0530

    Time: 4 ms (36.56%), Space: 8.8 MB (9.70%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,25 +11,43 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-     ListNode* temp=head;
-        int cnt=0;
-        while(temp!=nullptr)
+     // ListNode* temp=head;
+     //    int cnt=0;
+     //    while(temp!=nullptr)
+     //    {
+     //        cnt++;
+     //        temp=temp->next;
+     //    }
+     //    cnt=(cnt/2)+1;
+     //    temp=head;
+     //    int k=0;
+     //    while(temp!=nullptr)
+     //    {
+     //        k++;
+     //        if(cnt==k){
+     //            head=temp;
+     //            break;
+     //        }
+     //        temp=temp->next;
+     //    }
+     //   return head; 
+        
+       // ListNode* slow =  head;
+       //   ListNode* fast =head;
+       //  while(fast->next != NULL && fast != NULL){
+       //      slow=slow->next;
+       //      fast=fast->next->next;
+       //  }
+       //  return slow;
+        
+         ListNode *s=head;
+        ListNode *f=head;
+        while(f!=NULL&&f->next!=NULL)
         {
-            cnt++;
-            temp=temp->next;
+            s=s->next;
+            f=f->next->next;
         }
-        cnt=(cnt/2)+1;
-        temp=head;
-        int k=0;
-        while(temp!=nullptr)
-        {
-            k++;
-            if(cnt==k){
-                head=temp;
-                break;
-            }
-            temp=temp->next;
-        }
-       return head; 
+        return s;
+        
     }
 };
