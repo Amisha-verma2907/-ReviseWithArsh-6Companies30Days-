7d781f423ff2aa5165e3742a57212fae028e27a0
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-04-19 19:57:29 +0530

    Time: 4 ms (39.97%), Space: 15.2 MB (5.44%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+//     void elements(vector<int> nums,int p , int n ,vector<int> temp , vector<vector<int>> &ans){
+
+//         if(p==n){
+//             ans.push_back(temp);
+//             return ;
+//         }
+//         elements(nums,p+1,n,temp,ans);
+//         temp.push_back(nums[p]);
+//         elements(nums,p+1,n,temp,ans);
+
+
+//     }
+    void print(vector<int> nums,vector<int> ele,vector<vector<int>>&ans,int ind,int n){
+        if(ind == n){
+            ans.push_back(ele);
+            return;
+        }
+        ele.push_back(nums[ind]);
+        print(nums,ele,ans,ind+1,n);
+        ele.pop_back();
+        print(nums,ele,ans,ind+1,n);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // int n=nums.size();
+        // int p=0;
+        // vector<vector<int>> ans;
+        // vector<int> temp;
+        // elements(nums,p,n,temp,ans);
+        // return ans;
+        
+        vector<vector<int>>ans;
+        vector<int>ele;
+        int n=nums.size();
+        print(nums,ele,ans,0,n);
+        return ans;
+
+    }
+};
