9f4df8a4745fd89f55cb1b46d1080348c1662098
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-04-26 01:14:53 +0530

    Time: 2 ms (60.66%), Space: 8.8 MB (88.30%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,51 @@
+// class Solution {
+// public:
+//     void permutation(vector<int>& nums,vector<vector<int>> &ans, vector<int> &v,int freq[]){
+//         if(v.size() == nums.size()){
+//             ans.push_back(v);
+//             return;
+//         }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(!freq[i]){
+//                 freq[i]=1;
+//                 v.push_back(nums[i]);
+//                 permutation(nums,ans,v,freq);
+//                 v.pop_back();
+//                 freq[i]=0;
+//             }
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> v;
+//         int freq[nums.size()];
+//         for(int i=0;i<nums.size();i++){
+//             freq[i] = 0 ;         
+//                  }
+//         permutation(nums,ans,v,freq);
+//         return ans;
+//      }
+// };
+
+
 class Solution {
 public:
-    void permutation(vector<int>& nums,vector<vector<int>> &ans, vector<int> &v,int freq[]){
-        if(v.size() == nums.size()){
-            ans.push_back(v);
+    void recurPermute(int ind,vector<int>& nums, vector<vector<int>>&ans){
+        if(nums.size() == ind){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(!freq[i]){
-                freq[i]=1;
-                v.push_back(nums[i]);
-                permutation(nums,ans,v,freq);
-                v.pop_back();
-                freq[i]=0;
-            }
+            swap(nums[ind],nums[i]);
+            recurPermute(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> v;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++){
-            freq[i] = 0 ;         
-                 }
-        permutation(nums,ans,v,freq);
+        vector<vector<int>>ans;
+        recurPermute(0,nums,ans);
         return ans;
-     }
+    }
 };
