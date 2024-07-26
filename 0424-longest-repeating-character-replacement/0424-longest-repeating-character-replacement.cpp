b1df63a2e2b7e408d62d0165aa9731569d2a5923
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-07-26 21:59:52 +0530

    Time: 8 ms (59.65%), Space: 9.7 MB (9.19%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maxLen = 0;
-        for(int i=0;i<s.size();i++)
-        {
-            int maxFreq = 0;
-            int hash[256] = {0};
-            for(int j=i;j<s.size();j++)
-            {
-                hash[s[j] - 'A']++;
-                maxFreq =  max(maxFreq, hash[s[j]- 'A']);
+        // brute force O(n2)
+//         int maxLen = 0;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             int maxFreq = 0;
+//             int hash[256] = {0};
+//             for(int j=i;j<s.size();j++)
+//             {
+//                 hash[s[j] - 'A']++;
+//                 maxFreq =  max(maxFreq, hash[s[j]- 'A']);
                 
-                int len = j-i+1;
-                int lenReverse = len - maxFreq;
-                if(lenReverse <= k)
-                {
-                  maxLen = max(maxLen,j-i+1);  
-                }else{
-                    break;
-                }
-            }
+//                 int len = j-i+1;
+//                 int lenReverse = len - maxFreq;
+//                 if(lenReverse <= k)
+//                 {
+//                   maxLen = max(maxLen,j-i+1);  
+//                 }else{
+//                     break;
+//                 }
+//             }
             
+//         }
+//         return maxLen;
+        
+        //better approach using sliding window O(n)
+        
+        int l=0, r=0, maxLen = 0;
+        unordered_map<char,int>mp;
+        int maxFreq = 0;
+        while(r<s.size())
+        { 
+            mp[s[r]]++;
+            maxFreq = max(maxFreq,mp[s[r]]);
+            
+            int lenReverse = (r-l+1) - maxFreq;
+            if(lenReverse<=k)
+            {
+                maxLen=max(maxLen,r-l+1);
+            }else{
+                mp[s[l]]--;
+                l++;
+            }
+            r++;
         }
         return maxLen;
     }
