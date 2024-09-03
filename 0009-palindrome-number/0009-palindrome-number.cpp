5fc098dab793a8cf4680ee196d143295f33eb228
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-09-03 23:37:19 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (37.03%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+     if(x<0){
+            return false;
+        }
+        int num = x;
+        long sum = 0;
+        while(num != 0){
+            int a = num%10;
+            num /= 10;
+            sum = (sum*10)+a;
+        }
+        if(sum == x){
+            return true;
+        }
+        return false;
+    
+    }
+};
