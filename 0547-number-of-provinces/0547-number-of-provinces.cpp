4a974a61e9c86580b04bcf8b3514ffa7d94d4bb3
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-09-17 23:35:23 +0530

    Time: 25 ms (29.28%), Space: 19.7 MB (41.82%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,43 @@
 class Solution {
-public:
-void dfs(int j,vector<bool> &vis,vector<int> adj[]){
-        vis[j]=true;
-        for(auto x:adj[j]){
-            if(!vis[x]){
-                dfs(x,vis,adj);
+private:
+    // DFS function to explore all connected cities
+    void dfs(int city, vector<int>& vis, vector<int> adjLs[]) {
+        vis[city] = 1;  // Mark the current city as visited
+        
+        // Visit all the adjacent cities
+        for(auto it : adjLs[city]) {
+            if(!vis[it]) {
+                dfs(it, vis, adjLs);
             }
         }
     }
+
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-       
-        int n=(int)isConnected.size();
-        const int N=444;
-        vector<int> adj[N];
-        vector<bool> vis(N);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 and i!=j){
-                    adj[i].push_back(j);
+        int n = isConnected.size();
+        vector<int> adjLs[n];  // Create adjacency list
+        
+        // Build the adjacency list from the isConnected matrix
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isConnected[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
                 }
             }
         }
-        vis.assign(n+4,false);
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj);
-                cnt++;
+        
+        vector<int> vis(n, 0);  // Initialize the visited array
+        int provinces = 0;  // To count the number of provinces
+        
+        // Loop through each city and perform DFS if it's not visited
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                provinces++;  // New province found
+                dfs(i, vis, adjLs);  // Explore all cities in this province
             }
         }
-        return cnt;
+        
+        return provinces;  // Return the number of provinces
     }
 };
