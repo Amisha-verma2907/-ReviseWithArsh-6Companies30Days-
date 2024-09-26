a0872aaee2fa69d6bb11be25e005abc7bc3a5715
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-09-26 20:34:40 +0530

    Time: 15 ms (73.07%), Space: 18.2 MB (41.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,51 +1,57 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];  // adjacency list
+        vector<int>adj[numCourses];
         
-        // Build the graph
-        for(int i = 0; i < prerequisites.size(); i++) {
-            int course = prerequisites[i][0];
-            int prereq = prerequisites[i][1];
-            adj[prereq].push_back(course);  // prereq -> course
+        int row = prerequisites.size();
+        
+        for(int i=0;i<row;i++)
+        {
+                int first = prerequisites[i][0];
+                int second = prerequisites[i][1];
+             adj[second].push_back(first);
         }
         
-        // Indegree array to count prerequisites for each course
-        vector<int> indegree(numCourses, 0);
         
-        // Calculate indegrees
-        for(int i = 0; i < numCourses; i++) {
-            for(auto it : adj[i]) {
+        vector<int>indegree(numCourses,0);
+        
+        for(int i=0;i<numCourses;i++)
+        {
+            for(auto it:adj[i]){
                 indegree[it]++;
             }
         }
         
-        // Queue to perform BFS (courses with no prerequisites)
-        queue<int> q;
-        for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
+        
+        queue<int>q;
+        
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i] == 0)
+            {
                 q.push(i);
             }
         }
         
-        int count = 0;  // Number of courses that can be completed
+        int count = 0;
         
-        // BFS
-        while(!q.empty()) {
-            int curr = q.front();
+        while(!q.empty()){
+            int top = q.front();
             q.pop();
-            count++;
             
-            // Reduce indegree for all dependent courses
-            for(auto it : adj[curr]) {
+            count++;
+            for(auto it:adj[top]){
                 indegree[it]--;
-                if(indegree[it] == 0) {
+                if(indegree[it] == 0){
                     q.push(it);
                 }
             }
         }
         
-        // If count equals numCourses, all courses can be finished
-        return count == numCourses;
+        if(count == numCourses){
+            return true;
+        }
+        
+        return false;
     }
 };
