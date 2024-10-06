f5e0c35b8892c70f0157e58f5bedc6bcc7ae55a9
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-10-06 17:49:43 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (68.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int climb(int dp[],int n){
-        if(n<=2){
-            return n;
+        // if(n<=2){
+        //     return n;
+        // }
+        if(n == 1)
+        {
+            return 1;
+        }
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
         }
         
-        if(dp[n] != -1){
-            return dp[n];
-        }
+        return dp[n];
         
-        return dp[n] = climb(dp,n-1) + climb(dp,n-2);
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+        
+//         return dp[n] = climb(dp,n-1) + climb(dp,n-2);
         
         
     }
