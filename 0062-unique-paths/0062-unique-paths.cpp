dbc75d813ca7dcdca51ec3794fa3ab6f660d9d64
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-10-21 12:21:13 +0530

    Time: 0 ms (100.00%), Space: 8 MB (51.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -20,8 +20,34 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         
-        return paths(m-1,n-1,dp);
+        dp[0][0] = 1;
+        
+         for (int i = 0; i < m; i++) {
+            dp[i][0] = 1; // Only one way to reach any cell in the first column (move down)
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1; // Only one way to reach any cell in the first row (move right)
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                int up = 0;
+                int left = 0;
+                
+                if(i>0) up = dp[i-1][j];
+                
+                if(j>0) left = dp[i][j-1];
+                
+                dp[i][j] = up + left;
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
+        //return paths(m-1,n-1,dp);
     }
 };
