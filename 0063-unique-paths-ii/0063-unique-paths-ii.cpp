33b20b4c12c2491d9e867cb0c80ef0b68ef20ce5
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-10-22 12:06:16 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (6.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -31,6 +31,36 @@ public:
         
         vector<vector<int>>dp(m,vector<int>(n,-1));
         
-        return count(obstacleGrid,m-1,n-1,dp);
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else if(i == 0 && j==0)
+                {
+                    dp[i][j] =1;
+                }
+                else{
+                    int up =0;
+                    int left =0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    
+                    dp[i][j] = up + left;
+                }
+            }
+           
+        }
+        
+        return dp[m-1][n-1];
+       // return count(obstacleGrid,m-1,n-1,dp);
     }
 };
