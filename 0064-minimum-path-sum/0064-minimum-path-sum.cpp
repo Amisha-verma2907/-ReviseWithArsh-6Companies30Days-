01b13a8091ac596bb86437bee833915af146ed58
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-10-23 10:56:38 +0530

    Time: 0 ms (100.00%), Space: 14.8 MB (67.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -29,7 +29,32 @@ public:
         // Initialize a DP table with -1
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
+        dp[0][0] = grid[0][0];
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0] = grid[0][0];
+                }
+                else{
+                    int up = INT_MAX;
+                    int left = INT_MAX;
+                    
+                    if(i> 0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    
+                    dp[i][j] = grid[i][j] + min(up,left);
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
         // Start the recursive sum calculation from the bottom-right corner
-        return sum(grid, m - 1, n - 1, dp);
+       // return sum(grid, m - 1, n - 1, dp);
     }
 };
