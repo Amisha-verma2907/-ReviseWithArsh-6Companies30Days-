4a9a10728f9011ae7a0035a4d59c78cd8f7c2bbb
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-13 19:32:09 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (52.78%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-
-//     void elements(vector<int> nums,int p , int n ,vector<int> temp , vector<vector<int>> &ans){
-
-//         if(p==n){
-//             ans.push_back(temp);
-//             return ;
-//         }
-//         elements(nums,p+1,n,temp,ans);
-//         temp.push_back(nums[p]);
-//         elements(nums,p+1,n,temp,ans);
-
-
-//     }
-    void print(vector<int> nums,vector<int> &ele,vector<vector<int>>&ans,int ind){
+    void s(vector<int>& nums,int ind,vector<vector<int>> &ans,vector<int>&a){
         if(ind == nums.size()){
-            ans.push_back(ele);
+            ans.push_back(a);
             return;
         }
-        ele.push_back(nums[ind]);
-        print(nums,ele,ans,ind+1);
-        ele.pop_back();
-        print(nums,ele,ans,ind+1);
         
+        //take
+        a.push_back(nums[ind]);
+        
+        s(nums,ind+1,ans,a);
+        
+        a.pop_back();
+        //not take
+        s(nums,ind+1,ans,a);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        // int n=nums.size();
-        // int p=0;
-        // vector<vector<int>> ans;
-        // vector<int> temp;
-        // elements(nums,p,n,temp,ans);
-        // return ans;
-        
         vector<vector<int>>ans;
-        vector<int>ele;
-        int n=nums.size();
-        print(nums,ele,ans,0);
+        vector<int>a;
+        s(nums,0,ans,a);
+        
         return ans;
-
     }
 };
