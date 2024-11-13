1d54fe3719240dd679144c9c5b65a2d53d93ee8a
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-13 17:07:21 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (15.05%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-       double ans =1.0;
-        long long nn=n;
-        if(nn<0) nn = -1 *nn;
-        while(nn){
-            if(nn%2 == 0){
-                x= x*x;
-                nn= nn/2;
-            }else{
-                ans = ans*x;
-                nn=nn-1;
-            }
+    double Pow(double x, long long n) {
+        if (n == 0) {
+            return 1.0;
         }
-        if(n < 0) ans = double(1.0) / double(ans);
-        return ans;
+        if (n < 0) {
+            return 1 / Pow(x, -n);
+        }
+        if (n % 2 == 0) {
+            double half = Pow(x, n / 2);
+            return half * half;
+        } else {
+            return x * Pow(x, n - 1);
+        }
+    }
+    
+    double myPow(double x, int n) {
+       return Pow(x, static_cast<long long>(n));
         
     }
 };
+
+
+
+// double ans =1.0;
+       //  long long nn=n;
+       //  if(nn<0) nn = -1 *nn;
+       //  while(nn){
+       //      if(nn%2 == 0){
+       //          x= x*x;
+       //          nn= nn/2;
+       //      }else{
+       //          ans = ans*x;
+       //          nn=nn-1;
+       //      }
+       //  }
+       //  if(n < 0) ans = double(1.0) / double(ans);
+       //  return ans;
