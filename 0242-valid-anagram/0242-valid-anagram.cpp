0f496d7d336635924e922abf3f7e29d5675b4cfb
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-14 20:51:32 +0530

    Time: 3 ms (66.34%), Space: 10.1 MB (16.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       unordered_map<char,int>mp1;
-        if(s.size()!=t.size())
+//        unordered_map<char,int>mp1;
+//         if(s.size()!=t.size())
+//         {
+//             return false;
+//         }
+//         for(int i=0;i<s.size();i++)
+//         {
+//             mp1[s[i]]++;
+//         }
+//         for(int i=0;i<t.size();i++)
+//         {
+//             mp1[t[i]]--;
+//         }
+//         for(auto i:mp1)
+//         {
+//             if(i.second!=0)
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        unordered_map<char,int>mp;
+        
+        for(char it: s)
         {
-            return false;
+            mp[it]++;
         }
-        for(int i=0;i<s.size();i++)
+        
+        for(char it:t)
         {
-            mp1[s[i]]++;
-        }
-        for(int i=0;i<t.size();i++)
-        {
-            mp1[t[i]]--;
-        }
-        for(auto i:mp1)
-        {
-            if(i.second!=0)
+            if(mp.find(it) != mp.end() )
             {
+                mp[it]--;
+                if(mp[it] == 0){
+                mp.erase(it);
+            }
+            }
+            else{
                 return false;
             }
         }
-        return true;
+        
+        
+        return mp.empty();
         
     }
 };
