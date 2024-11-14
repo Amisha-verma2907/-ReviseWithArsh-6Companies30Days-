6036c869255117ad05590fa4a09104d15d7a057b
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-14 20:33:40 +0530

    Time: 0 ms (100.00%), Space: 27.3 MB (18.03%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    void reverse(vector<char>&s,int left,int right){
-        if(left>=right){
-            return;
-        }
+//     void reverse(vector<char>&s,int left,int right){
+//         if(left>=right){
+//             return;
+//         }
         
-        swap(s[left],s[right]);
-        reverse(s,left+1,right-1);
-    }
+//         swap(s[left],s[right]);
+//         reverse(s,left+1,right-1);
+//     }
     void reverseString(vector<char>& s) {
+        // int n = s.size();
+        // reverse(s,0,n-1);
+        
         int n = s.size();
-        reverse(s,0,n-1);
+        
+        reverse(s.begin(),s.end());
     }
 };
