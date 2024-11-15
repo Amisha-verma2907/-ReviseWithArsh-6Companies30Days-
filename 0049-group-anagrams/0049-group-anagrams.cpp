09329b25abfc4bc7e77fbac19c25b0844a222c36
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-15 12:20:17 +0530

    Time: 32 ms (21.47%), Space: 27.9 MB (18.15%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
+    string generateFrequency(string s){
+       int hash[256] = {0};
+        
+       for(int i=0;i<s.size();i++)
+       {
+           hash[s[i]]++;
+       }
+        
+        string t;
+        for(int i = 0;i<256;i++)
+        {
+            if(hash[i] > 0)
+            {
+                t+= (char)i;
+                t+=to_string(hash[i]);
+            }
+        }
+        
+        return t;
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
         vector<vector<string>>ans;
         
         unordered_map<string,vector<string>>mp;
         
         for(auto it:strs){
-            string sortedString = it;
+            string m = generateFrequency(it);
             
-            sort(sortedString.begin(),sortedString.end());
-            
-            mp[sortedString].push_back(it);
-            
+            mp[m].push_back(it);
         }
         
         for(auto it: mp)
@@ -20,6 +39,28 @@ public:
         }
         
         return ans;
-        
     }
 };
+
+
+
+//         vector<vector<string>>ans;
+        
+//         unordered_map<string,vector<string>>mp;
+        
+//         for(auto it:strs){
+//             string sortedString = it;
+            
+//             sort(sortedString.begin(),sortedString.end());
+            
+//             mp[sortedString].push_back(it);
+            
+//         }
+        
+//         for(auto it: mp)
+//         {
+//             ans.push_back(it.second);
+//         }
+        
+//         return ans;
+        
