0edd1d90dc3d427ee8231c6ec6e2f7b26ec1b771
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-20 20:46:49 +0530

    Time: 31 ms (35.61%), Space: 44.1 MB (17.67%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -27,10 +27,68 @@ public:
         return dp[ind][amount] = take + notTake;
     }
     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+    
+//     vector<vector<unsigned long long>> dp(n, vector<unsigned long long>(amount + 1, 0));
+    
+//     // Initialize base case
+//     for (int i = 0; i <= amount; i++) {
+//         if (i % coins[0] == 0) {
+//             dp[0][i] = 1; // If `i` is a multiple of coins[0]
+//         }
+//     }
+    
+//     // Fill the DP table
+//     for (int i = 1; i < n; i++) {
+//         for (int j = 0; j <= amount; j++) {
+//             unsigned long long notTake = dp[i - 1][j]; // Exclude current coin
+            
+//             unsigned long long take = 0;
+//             if (j >= coins[i]) {
+//                 take = dp[i][j - coins[i]]; // Include current coin
+//             }
+            
+//             dp[i][j] = take + notTake; // Total combinations
+//         }
+//     }
+    
+//     return dp[n - 1][amount]; 
+        
+        
+        //         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        
+//         return count(coins,amount,coins.size()-1,dp);
+        
+        
         int n = coins.size();
+    
+        vector<unsigned long long>prev(amount+1,0);
         
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+    
+    // Initialize base case
+    for (int i = 0; i <= amount; i++) {
+        if (i % coins[0] == 0) {
+            prev[i] = 1; // If `i` is a multiple of coins[0]
+        }
+    }
+    
+    // Fill the DP table
+    for (int i = 1; i < n; i++) {
+        vector<unsigned long long>cur(amount+1,0);
+        for (int j = 0; j <= amount; j++) {
+            unsigned long long notTake = prev[j]; // Exclude current coin
+            
+            unsigned long long take = 0;
+            if (j >= coins[i]) {
+                take = cur[j - coins[i]]; // Include current coin
+            }
+            
+            cur[j] = take + notTake; // Total combinations
+        }
         
-        return count(coins,amount,coins.size()-1,dp);
+        prev = cur;
+    }
+    
+    return prev[amount]; 
     }
 };
