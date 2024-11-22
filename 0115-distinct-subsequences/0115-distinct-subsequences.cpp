bce534af745382820343536920de4170e35a9e78
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-22 17:50:43 +0530

    Time: 34 ms (44.18%), Space: 43 MB (32.33%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -24,8 +24,37 @@ public:
         int n1 = s.size();
         int n2 = t.size();
         
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        //tabulation
         
-        return count(s,t,n1-1,n2-1,dp);
+        vector<vector<unsigned long long>>dp(n1+1,vector<unsigned long long>(n2+1,0));
+        
+         for(int i=0;i<=n1;i++)
+        {
+            dp[i][0] = 1;
+        }
+        
+        for(int j=1;j<=n2;j++)
+        {
+            dp[0][j] = 0;
+        }
+        
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++)
+            {
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        
+        return dp[n1][n2];
+        //memoization
+//         vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        
+//         return count(s,t,n1-1,n2-1,dp);
     }
 };
