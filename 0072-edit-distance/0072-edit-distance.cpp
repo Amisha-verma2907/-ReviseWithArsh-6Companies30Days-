d6bba24cc85ea8b09c45a53d2b16f8a6f1d09b72
Amisha verma <140652034+Amisha-verma2907@users.noreply.github.com>
2024-11-22 20:45:54 +0530

    Time: 3 ms (87.84%), Space: 13 MB (39.50%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -26,10 +26,44 @@ public:
     }
     int minDistance(string word1, string word2) {
        int n1= word1.size();
-        int n2 = word2.size();
+       int n2 = word2.size();
         
-        vector<vector<int>>dp(n1,vector<int>(n2+1,-1));
+        //tabulation
         
-       return count(word1,word2,n1-1,n2-1,dp);
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        
+        for(int i=0;i<=n1;i++)
+        {
+            dp[i][0] = i;
+        }
+        
+         for(int j=0;j<=n2;j++)
+        {
+            dp[0][j] = j;
+        }
+        
+        
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++)
+            {
+                
+            if(word1[i-1] == word2[j-1]){
+                  dp[i][j] = 0 + dp[i-1][j-1];
+            }else{
+            int insertO = 1 + dp[i][j-1];
+            int deleteO = 1 + dp[i-1][j];
+            int replace = 1+ dp[i-1][j-1];
+            
+             dp[i][j] = min(insertO,min(deleteO,replace));
+            } 
+            }
+        }
+        
+        return dp[n1][n2];
+        
+        //memoization
+//        vector<vector<int>>dp(n1,vector<int>(n2+1,-1));
+        
+//        return count(word1,word2,n1-1,n2-1,dp);
     }
 };
